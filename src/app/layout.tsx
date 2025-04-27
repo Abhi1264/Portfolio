bcbbdfc2b3fdf6aaefb36bed45fa16a5f3e0e416
Abhi1264 <abhinav.kumar.1264@gmail.com>
2025-04-28 00:50:22 +0530

    remove react scan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body className={manrope.className}>
